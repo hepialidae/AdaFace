ab785f835f5d92c38c3b10b92690c06ac06ee022
hepialidae <150652124+hepialidae@users.noreply.github.com>
2026-07-16 20:37:21 +0800

    context manager fix

--- inference.py
+++ inference.py
@@ -15,7 +15,8 @@ def load_pretrained_model(architecture='ir_50'):
     model = net.build_model(architecture)
     statedict = torch.load(adaface_models[architecture])['state_dict']
     model_statedict = {key[6:]:val for key, val in statedict.items() if key.startswith('model.')}
-    model.load_state_dict(model_statedict)
+    with torch.serialization.add_safe_globals([pytorch_lightning.callbacks.model_checkpoint.ModelCheckpoint]):
+        model.load_state_dict(model_statedict)
     model.eval()
     return model
 
